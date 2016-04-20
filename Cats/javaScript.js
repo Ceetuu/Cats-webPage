0d1c85703b04bdbd25cb7d730aa8098ca01b94d3
Ceetu <carlitos_c_1996@hotmail.com>
2016-04-19 21:07:00 -0500

    Added files via upload

---
+++ Cats/javaScript.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+  var i=0;
+
+  var fig = document.getElementById('cuadro');
+
+  fig.onclick=function(){ fig.textContent = "X"; };
+
+
+})
+
+
+
+
+/*
+
+fig.addEventListener("click", function(){  fig.textContent = document.write("X"); }, false);
+*/
+
+/*
+$(function(){
+  $('#cuadro').click( function(){
+    $('#relleno').toggleClass("show");
+  });
+});
+*/
+
+/*
+function checaGanador(){
+
+}
+*/
